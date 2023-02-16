0410f752574d2526c42cc7a862fe433aec2659cc
root <root@StephenOdero.localdomain>
2023-02-16 14:30:25 +0300

    Making changes to script 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,11 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
-	printf("Size of a char: %d bytes(s)\n",sizeof(a));
-	printf("Size of an int: %d bytes(s)\n",sizeof(b));
-	printf("Size of a long int: %d bytes(s)\n",sizeof(c));
-	printf("Size of a long long int: %d bytes(s)\n",sizeof(d));
-	printf("Size of a float: %d bytes(s)\n",sizeof(e));
+
+	printf("Size of a char: %d bytes(s)\n", sizeof(a));
+	printf("Size of an int: %d bytes(s)\n", sizeof(b));
+	printf("Size of a long int: %d bytes(s)\n", sizeof(c));
+	printf("Size of a long long int: %d bytes(s)\n", sizeof(d));
+	printf("Size of a float: %d bytes(s)\n", sizeof(e));
 	return (0);
 }
