ff93b0e263a8cc5231446ee03a3c33d6ec75e3e4
root <root@StephenOdero.localdomain>
2023-02-20 20:15:41 +0300

    Update on Hexadecimal counting

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+  * main - main function of the program
+  * Description - a program to print all the base 16 numbers
+  * Return: zero when the program runs without an error
+*/
+
+int main(void)
+{
+	int a;
+	int b = 97;
+
+	for (a = 0; a < 15; a++)
+	{
+		/* putchar('0'+a); */
+		if (a <= 9)
+		{
+			putchar('0' + a);
+		}
+		else if (a > 9)
+		{
+			putchar(b);
+			b++;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
