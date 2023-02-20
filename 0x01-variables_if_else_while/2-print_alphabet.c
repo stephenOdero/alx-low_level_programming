2a163a98fd28ece5f758f2850d28a840dd977e6c
root <root@StephenOdero.localdomain>
2023-02-20 11:41:24 +0300

    Printing lower case alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - main program
+  * Description - A program that prints the alphabets in lower case
+  * Return: Returns 0 if everything is working well
+*/
+
+int main(void)
+{
+	char my_char;
+	/* using for loop to go through a-z using ASCII */
+	for (my_char = 'a'; my_char <= 'z'; my_char++)
+	{
+		/* printf("%c\n", my_char); */
+		putchar(my_char);
+		putchar('\n');
+	}
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,3 @@
 0X01 C Variables, If, Else and While
 0-positive_or_negative.c: a program to print out whether the randomly generated number is positive or negative.
+2-print_alphabet.c: A program to print all the alphabets in lowercase
