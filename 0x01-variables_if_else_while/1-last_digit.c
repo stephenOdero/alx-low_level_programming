b822fa74cc891ecd9fc3c97c6caf3ffb4729e9cc
root <root@StephenOdero.localdomain>
2023-02-20 16:53:43 +0300

    Making corrections to last digit.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
 /* betty style doc for function main goes there */
 /**
   * main - the main entry to the program
-  * Description - A program to print the last digit 
+  * Description - A program to print the last digit
   * Return: Returns 0 if the program works well.
 */
 
@@ -17,7 +17,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	
+	if (n < 0)
+	{
+		n=-n;
+		/* Using only the positive values of n */
+	}
 	/* To find the last digit, use the Num % 10 to get */
 	printf("%d\n", n % 10);
 
