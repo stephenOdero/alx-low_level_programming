d9208e153a4f5dffd24d02745bb876ea0976bb93
root <root@StephenOdero.localdomain>
2023-03-05 11:33:14 +0300

    Checking the sign of an integer

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+  * print_sign- prints the sign of a number
+  * @n: number to check
+  * Return: 1 and prints + if n is greater than zero
+  * returns 0 and prints 0 if n is zero
+  * returns -1 and prints - if n is less than zero
+  */
+
+int print_sign(int n)
+{
+	/* If n is greater than 0 */
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	/* If n is equal to zero */
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	/* if n is less than zero */
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -4,3 +4,4 @@ Directory on my excercises in Functions and Nested loops
 2-print_alphabet_x10: A function to print the alphabet ten times followed by a newline
 3-islower: A function checks for lowercase characters
 4-isalpha: A function that checks for alphabetic character. Returns 1 if c is a letter and 0 otherwise
+5-sign: A function that prints the sign of a number
