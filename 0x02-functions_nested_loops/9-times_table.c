de939bedc787817ebc672dcd83f453a9beb35d1c
root <root@StephenOdero.localdomain>
2023-03-05 17:01:42 +0300

    Multiplication table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  * times_table- function that prints the 9 times table
+  * starting with 0
+  */
+
+void times_table(void)
+{
+	/* Using two counters to do my math */
+	int value;
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			value = i * j;
+			/* Printing the values */
+			if ((value / 10) != 0)
+				_putchar((value / 10) + '0');
+			else if ((value / 10) == 0)
+				_putchar(' ');
+			_putchar((value % 10) + '0');
+			if (j == 9)
+				continue;
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -8,3 +8,4 @@ Directory on my excercises in Functions and Nested loops
 6-abs: A function that computes the absolute value of an integer
 7-print-last_digit: A function that prints the last digit of a number
 8-24_hours: A function that prints every minute of the day of Jack Bauer from 00:00 to 23:59
+9-times_table: A function that prints the 9 times table, starting with 0
