17d2fe3d843bad9c115499929dcd222c2a0d0199
root <root@StephenOdero.localdomain>
2023-03-07 20:48:40 +0300

    making subtle changes

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,6 +22,8 @@ void times_table(void)
 				_putchar((value / 10) + '0');
 			else if ((value / 10) == 0)
 				_putchar(' ');
+			else if ((i == 0) && (j ==0))
+				_putchar('0');
 			_putchar((value % 10) + '0');
 			if (j == 9)
 				continue;
